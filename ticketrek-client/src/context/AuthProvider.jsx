fdd5080a0ddad6d0a996e8be07e18c6c39048d5e
unknown <gazinafisrafi.gnr@gmail.com>
2025-02-12 01:13:12 +0600

    firebase initiated

--- ticketrek-client/src/context/AuthProvider.jsx
+++ ticketrek-client/src/context/AuthProvider.jsx
@@ -1,22 +1,49 @@
 /* eslint-disable react-refresh/only-export-components */
 /* eslint-disable react/prop-types */
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import {createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut, updateProfile} from 'firebase/auth'
+import { app } from "../Firebase/firebase.config";
 
 export const AuthContext = createContext()
 const AuthProvider = ({children}) => {
     const basicUser = {
         name: 'John Doe',
         email: 'test@mail.com',
-        role: 'customer'
+        role: 'admin'
     }
+    const auth = getAuth(app);
     const [user, setUser] = useState(basicUser);
-    const [loading, setLoading] = useState(false)
+    const [loading, setLoading] = useState(true);
+    const register = (email, password) =>{
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+    const login = (email, password) =>{
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+    const getProfile = (displayName) =>{
+        return updateProfile(auth.currentUser, {displayName})
+    }
+    const logOut = () =>{
+        return signOut(auth)
+    }
     const value = {
         user,
         setUser,
         loading,
-        setLoading
+        setLoading,
+        register,
+        login,
+        getProfile,
+        logOut
     }
+    useEffect(() =>{
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) =>{
+            if(currentUser){
+                setLoading(false)
+            }
+        })
+        return () => unsubscribe()
+    },[])
     return (
         <AuthContext.Provider value={value}>
             {children}
